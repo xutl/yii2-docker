b482b77c0d1e699e20d8591041888d127dc86058
Tobias Munk <schmunk@usrbin.de>
2017-03-10 12:11:19 +0100

    cleanup build & test script

--- tests/run.sh
+++ tests/run.sh
@@ -1,28 +1,25 @@
 #!/usr/bin/env bash
 
 set -e
-set -v
 
-COMPOSE_FILE=./docker-compose.yml
-
-if [ -n ${CI_BUILD_NAME} ]
-then
-    image=${CI_BUILD_NAME}
-else
-    image=php-${VERSION}-${VARIANT}
-fi
+image=php-${VERSION}-${VARIANT}
 
 echo "Building & testing: ${image}"
 
 docker-compose build --pull ${image}
 
+# Download Yii2 for running requirement checks & tests
 curl https://github.com/yiisoft/yii2/archive/master.tar.gz -o yii.tar.gz -L
 tar -xzf yii.tar.gz
 
+# Install framework dependencies
+docker-compose run --rm -w /tests/yii2-master ${image} \
+    composer install
+
+# Run requirement checks
 docker-compose run --rm ${image} \
     php /tests/requirements.php
 
-docker-compose run --rm -w /tests/yii2-master ${image} \
-    composer install
+# Run subset of test-suite
 docker-compose run --rm -w /tests/yii2-master ${image} \
     vendor/bin/phpunit -v --exclude caching,db,data --log-junit tests/_junit/test.xml
